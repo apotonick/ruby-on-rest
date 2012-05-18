cec8d7c9afa945a05852e949e2c85da20df28bfc
Nick Sutterer <apotonick@gmail.com>
2012-05-18 15:32:18 +0200

    implemented the same with a generic PaginationRepresenter.

--- rails/app/representers/bowl_page_representer.rb
+++ rails/app/representers/bowl_page_representer.rb
@@ -1,24 +1,39 @@
-module BowlPageRepresenter
+module PaginationRepresenter
   include Roar::Representer::JSON
   include Roar::Representer::Feature::Hypermedia
-  
+    
   property :total_entries
-  collection :items, :extend => FruitRepresenter
   
   link :self do |opts|
-    bowl_url(opts[:bowl], :page => current_page)
+    page_url(opts[:model], :page => current_page)
   end
   
   link :next do |opts|
-    bowl_url(opts[:bowl], :page => next_page) if next_page
+    page_url(opts[:model], :page => next_page) if next_page
   end
   
   link :previous do |opts|
-    bowl_url(opts[:bowl], :page => previous_page) if previous_page
+    page_url(opts[:model], :page => previous_page) if previous_page
   end
   
   def items
     self
   end
   
+  def page_url(*)
+    raise "Implement me."
+  end
+end
+
+
+module BowlPageRepresenter
+  include Roar::Representer::JSON
+  include Roar::Representer::Feature::Hypermedia
+  include PaginationRepresenter
+  
+  collection :items, :extend => FruitRepresenter
+  
+  def page_url(*args)
+    bowl_url(*args)
+  end
 end
--- rails/test/unit/bowl_page_representer_test.rb
+++ rails/test/unit/bowl_page_representer_test.rb
@@ -9,18 +9,18 @@ class BowlPageRepresenterTest < ActiveSupport::TestCase
   test "page 1 contains link to next" do
     @page = @bowl.fruits.paginate(:page => 1, :per_page => 2)
     
-    assert_equal "{\"total_entries\":5,\"items\":[{\"title\":\"Apple\",\"links\":[{\"rel\":\"self\",\"href\":\"http://y/fruits/Apple\"}]},{\"title\":\"Orange\",\"links\":[{\"rel\":\"self\",\"href\":\"http://y/fruits/Orange\"}]}],\"links\":[{\"rel\":\"self\",\"href\":\"http://y/bowls/1?page=1\"},{\"rel\":\"next\",\"href\":\"http://y/bowls/1?page=2\"}]}", @page.extend(BowlPageRepresenter).to_json(:bowl => @bowl)
+    assert_equal "{\"total_entries\":5,\"links\":[{\"rel\":\"self\",\"href\":\"http://y/bowls/1?page=1\"},{\"rel\":\"next\",\"href\":\"http://y/bowls/1?page=2\"}],\"items\":[{\"title\":\"Apple\",\"links\":[{\"rel\":\"self\",\"href\":\"http://y/fruits/Apple\"}]},{\"title\":\"Orange\",\"links\":[{\"rel\":\"self\",\"href\":\"http://y/fruits/Orange\"}]}]}", @page.extend(BowlPageRepresenter).to_json(:model => @bowl)
   end
   
   test "page 2 contains link to next and previous" do
     @page = @bowl.fruits.paginate(:page => 2, :per_page => 2)
     
-    assert_equal "{\"total_entries\":5,\"items\":[{\"title\":\"Banana\",\"links\":[{\"rel\":\"self\",\"href\":\"http://y/fruits/Banana\"}]},{\"title\":\"Mango\",\"links\":[{\"rel\":\"self\",\"href\":\"http://y/fruits/Mango\"}]}],\"links\":[{\"rel\":\"self\",\"href\":\"http://y/bowls/1?page=2\"},{\"rel\":\"next\",\"href\":\"http://y/bowls/1?page=3\"},{\"rel\":\"previous\",\"href\":\"http://y/bowls/1?page=1\"}]}", @page.extend(BowlPageRepresenter).to_json(:bowl => @bowl)
+    assert_equal "{\"total_entries\":5,\"links\":[{\"rel\":\"self\",\"href\":\"http://y/bowls/1?page=2\"},{\"rel\":\"next\",\"href\":\"http://y/bowls/1?page=3\"},{\"rel\":\"previous\",\"href\":\"http://y/bowls/1?page=1\"}],\"items\":[{\"title\":\"Banana\",\"links\":[{\"rel\":\"self\",\"href\":\"http://y/fruits/Banana\"}]},{\"title\":\"Mango\",\"links\":[{\"rel\":\"self\",\"href\":\"http://y/fruits/Mango\"}]}]}", @page.extend(BowlPageRepresenter).to_json(:model => @bowl)
   end
   
   test "page 3 contains link to next" do
     @page = @bowl.fruits.paginate(:page => 3, :per_page => 2)
     
-    assert_equal "{\"total_entries\":5,\"items\":[{\"title\":\"Cherry\",\"links\":[{\"rel\":\"self\",\"href\":\"http://y/fruits/Cherry\"}]}],\"links\":[{\"rel\":\"self\",\"href\":\"http://y/bowls/1?page=3\"},{\"rel\":\"previous\",\"href\":\"http://y/bowls/1?page=2\"}]}", @page.extend(BowlPageRepresenter).to_json(:bowl => @bowl)
+    assert_equal "{\"total_entries\":5,\"links\":[{\"rel\":\"self\",\"href\":\"http://y/bowls/1?page=3\"},{\"rel\":\"previous\",\"href\":\"http://y/bowls/1?page=2\"}],\"items\":[{\"title\":\"Cherry\",\"links\":[{\"rel\":\"self\",\"href\":\"http://y/fruits/Cherry\"}]}]}", @page.extend(BowlPageRepresenter).to_json(:model => @bowl)
   end
 end
